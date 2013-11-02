fe5a0547f1faf98493c077de5c0a8c8a658a5856
PerseP <persep@gmail.com>
2013-11-02 14:05:22 +0100

    Read from index.html

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fs = require('fs');
+var infile = "index.html"; 
+var readfile = fs.readFileSync(infile);
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(readfile.toString());
 });
 
 var port = process.env.PORT || 5000;
